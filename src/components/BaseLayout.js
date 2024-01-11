f4638393191d8fd8e2c67ddac9f12cb1ecbda12a
rekha0suthar <rekha0suthar@gmail.com>
2024-01-11 15:58:03 +0530

    url related issue fixed

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -36,7 +36,7 @@ export default function BaseLayout() {
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'/'} element={<Home/>}/>
+                  <Route exact path={'/portfolio'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/projects'} element={<Portfolio/>}/>
                </Routes>
